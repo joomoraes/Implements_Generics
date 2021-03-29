6a44a2df63afc7af51d1ffa57e6d20987be9d76c
João Alves <joao.alves@liberali.local>
2021-03-29 09:32:51 -0400

    Implementação do tipo generics em C#

---
+++ PrinterService.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Generics_1
+{
+    class PrinterService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if(_count == 10) {
+                System.Console.WriteLine("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 10)
+            {
+                System.Console.WriteLine("PrintService is full");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            System.Console.WriteLine("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                System.Console.WriteLine(_values[i] + ", ");
+            }
+            if(_count > 0)
+            {
+                System.Console.WriteLine(_values[_count -1]);
+            }
+            System.Console.WriteLine("]");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Generics_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterService<string> probjectService = new PrinterService<string>();
+
+            Console.WriteLine("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                probjectService.AddValue(x);
+            }
+
+            //int a = (int)PrinterService.First();
+            //int b = a + 2;
+            //Console.WriteLine(b);
+
+            probjectService.Print();
+            Console.WriteLine("Firts: " + probjectService.First());
+        }
+    }
+}
